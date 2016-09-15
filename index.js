e376f1c585de9738c73d4f80ca3fd40e3745d973
Chris Tsantiris <Chris.Tsantiris@gmail.com>
2016-09-15 08:52:16 -0400

    Fixed typo added gitignore

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 'use strict'
 
-const http = require('http')
-const express = require('express')
-const socketio = require('socket.io')
-const Twit = require('twit')
-const EventEmitter = require('events')
+const config = require('./config')
 const debugBot = require('debug')('bot')
 const debugServer = require('debug')('server')
-const config = require('./config')
+const EventEmitter = require('events')
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const Twit = require('twit')
+
+debugBot('starting bot')
+const client = new Twit(config)
+
+const stream = client.stream('statuses/filter', {
+  track: ['javascript', 'nodeschool', 'teach', 'suncoast']
+})
 
 // Server
 const events = new EventEmitter()
@@ -17,8 +24,8 @@ app.use(express.static('public'))
 
 const io = socketio(server)
 
-io.on('connection', (socket) => {
-  events.on('retweet', (message) => {
+io.on('connection', socket => {
+  events.on('retweet', message => {
     socket.emit('retweet', message)
   })
 })
@@ -27,24 +34,18 @@ server.listen(3000, () => {
   debugServer('Server listening!')
 })
 
-const client = new Twit(config)
-
-const stream = client.stream('statuses/filter', {
-  track: [ 'javascript', 'nodeschool' ]
-})
-
-stream.on('tweet', (tweet) => {
+// Bot
+stream.on('tweet', tweet => {
   debugBot('received a tweet')
-  if (tweet.text.match(/@christsantiris/i)) {
-    debugBot('retweeted %s', tweet.text)
+  if (tweet.text.match(/@christsantiris/)) {
     client.post('statuses/retweet/:id', { id: tweet.id_str })
     events.emit('retweet', {
       by: tweet.user.screen_name,
       text: tweet.text
     })
-  }
-
-  if (tweet.text.match(/@suncoastjs/)) {
+    debugBot('retweeted %s', tweet.text)
     client.post('favorites/create', { id: tweet.id_str })
+    debugBot('liked %s', tweet.text)
+    return
   }
 })
